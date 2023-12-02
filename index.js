ef0ba846abf5eaa0193711259094262165275108
abdulalh-al-mohammad <abdullahalmuhammad6@mail.com>
2023-12-02 20:38:28 +0600

    header bg and text color change on scroll

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 function changeNavbarColorOnScroll(){
+  console.log("scrooled");
     const navId = document.getElementById('navbar');
     const scrollPosition = window.scrollY;
 
     if(scrollPosition > 100){
       navId.style.backgroundColor = 'white';
-      // navId.classList.remove('.demo .nav-link:focus, .nav-link');
+      navId.classList.remove('.demo .nav-link:focus, .nav-link');
       navId.classList.add('text-dark');
     }
     else{
       navbar.style.backgroundColor = 'transparent';
     }
   }
-  window.addEventListener('scroll', changeNavbarColorOnScroll)
+  // window.addEventListener('scroll', changeNavbarColorOnScroll);
 
-  function inputField(restaurent){
-    restaurent = '';
-  }
-  inputField()
+
+  window.addEventListener('scroll', function (){
+    const navId = document.getElementById('navbar');
+    const mainLogo = document.querySelector('.main-logo');
+    const stickyLogo = document.querySelector('.sticky-logo');
+    const navLinks = document.querySelectorAll("#navbar .nav-link");
+    const scrollPosition = window.scrollY;
+
+    if(scrollPosition > 100){
+      navId.classList.add("bg-white");
+      mainLogo.classList.add("d-none");
+      stickyLogo.classList.remove("d-none")?.add("d-block");
+      navLinks.forEach((navLink) => navLink.classList.add("text-black"));
+      // navId.classList.remove('.demo .nav-link:focus, .nav-link');
+    } else{
+      navId.classList.remove("bg-white");
+      mainLogo.classList.remove("d-none");
+      stickyLogo.classList.add("d-none");
+      navLinks.forEach((navLink) => navLink.classList.remove("text-black"));
+    }
+  })
